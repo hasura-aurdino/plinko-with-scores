388251d91eab2d07d3761a7a6ea85486e651bec7
hasura-aurdino <73148676+hasura-aurdino@users.noreply.github.com>
2021-10-01 04:08:26 +0530

    Add files via upload

---
+++ 5B322757FC9495E4D306102F7152430E-master/5B322757FC9495E4D306102F7152430E-master/sketch.js
@@ -0,0 +1,56 @@
+const Engine = Matter.Engine;
+const   World = Matter.World;
+ const Events = Matter.Events;
+ const  Bodies = Matter.Bodies;
+ 
+
+var plinkos = [];
+var divisions =[];
+var divisionHeight=300;
+var score =0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+   
+
+    
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ //text("Score : "+score,20,30);
+  Engine.update(engine);
+  ground.display();
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   
+ 
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+}
